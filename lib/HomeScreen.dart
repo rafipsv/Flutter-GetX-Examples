ec1fb69b6608bc2bd27aa41472250ef640765295
Rafi Ahmed <rafidiya143@gmail.com>
2023-08-08 21:07:55 +0600

    Getx Initial Setup

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
+    );
+  }
+}
