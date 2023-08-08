80f22493b441b6445de92efd24df703d3b27eda9
Rafi Ahmed <rafidiya143@gmail.com>
2023-08-08 21:14:19 +0600

    Snackbar Added

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -10,7 +10,9 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Get.snackbar("Muhammad", "Testing Snackbar");
+        },
       ),
     );
   }
